51080f6a252241c5a7ed43573ce14b012170965c
Mona <murban@hs-mittweida.de>
2021-12-25 15:03:46 +0100

    Duc plz help lol

--- day16/taskMona.py
+++ day16/taskMona.py
@@ -22,9 +22,13 @@ binaryvalues = {
 }
 for line in lines:
     for char in line:
-       binarycode.append(binaryvalues[char])
+        binary = binaryvalues[char]
+        for numb in binary:
+            binarycode.append(numb)
 
-def getVersionNumber(binaryString):
+print(binarycode)
+
+def getVersionNumber():
     versionbinary = "0"
 
     for char in range(3):
@@ -41,9 +45,10 @@ def getVersionNumber(binaryString):
 
     versionID = list(binaryvalues.keys())[list(binaryvalues.values()).index(versionIDbinary)]
 
-    if int(versionID) == 4:
-        literalvalue = ""
+    if int(versionID[0]) == 4:
+       
         while binarycode[0] != 1:
+            literalvalue = ""
             binarycode.pop(0)
 
             for char in range(4):
@@ -53,6 +58,8 @@ def getVersionNumber(binaryString):
             for char in range(4):
                 literalvalue += binarycode[int(char)]
                 binarycode.pop(char)
+            value = list(binaryvalues.keys())[list(binaryvalues.values()).index(literalvalue)]
+            int(version[0]) += int(value[0])
 
     else:
         if binarycode[0] == 0:
@@ -67,12 +74,20 @@ def getVersionNumber(binaryString):
                 binarycode.pop(char)
             numbOfPackets = list(binaryvalues.keys())[list(binaryvalues.values()).index(binNumbOfPacketes)]
 
-            for i in range(int(numbOfPackets)):
+            for i in range(int(numbOfPackets[0])):
+                literalvalue = ""
                 for char in range(7):
                     binarycode.pop(char)
                 for char in range(4):
-                    binNumbOfPacketes += binarycode[char]
+                    literalvalue += binarycode[char]
                     binarycode.pop(char)
+                value = list(binaryvalues.keys())[list(binaryvalues.values()).index(literalvalue)]
+        
+                int(version[0]) += int(value[0])
+    return version[0]
+
+print(getVersionNumber())
+                
 
 
 
